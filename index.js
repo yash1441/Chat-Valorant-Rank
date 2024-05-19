9a96f73e0aff0ecab4d122100c37e79de73ca638
Simon <yash1441@yahoo.com>
2024-05-19 20:46:43 +0530

    Updated to latest model

--- index.js
+++ index.js
@@ -1,38 +1,47 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
+const fs = require("fs");
 const HenrikDevValorantAPI = require('unofficial-valorant-api');
-const vapi = new HenrikDevValorantAPI();
 
-const fs = require('fs');
-const myConsole = new console.Console(fs.createWriteStream('./output.txt'));
+const vapi = new HenrikDevValorantAPI(INPUT_YOUR_HENRIKDEV_KEY_HERE);
+const github = "https://github.com/yash1441/Chat-Valorant-Rank/";
+const myConsole = new console.Console(fs.createWriteStream('./output.txt', { flags: 'a' }));
 
 app.get('/', (req, res) => {
-	res.send('Stop snooping around >.>');
+	return res.send(`Please visit <a href="${github}">GitHub</a> to know how to use this API.`);
 });
 
-app.get('/valorant/:name/:tag', async (req, res, next) => {
+app.get('/valorant/:region?/:name/:tag', async (req, res, next) => {
 	const name = req.params.name;
 	const tag = req.params.tag;
+	let region = req.params.region;
+
+	if (name == "name" && tag == "tag") {
+		return res.send(`Please visit <a href="${github}">GitHub</a> to know how to use this API.`);
+	}
+
+	if (!region) region = 'ap';
 
 	const mmr_data = await vapi.getMMR({
 		version: 'v1',
-		region: 'ap',
+		region: region,
 		name: name,
-		tag: tag,
+		tag: tag
 	});
-	if (mmr_data.error)	{
+
+	if (mmr_data.error) {
 		res.send(`Error ${mmr_data.status}`);
-		return myConsole.log(`Username: ${name}#${tag} Error: ${mmr_data.status}`);
+		return myConsole.log(`${region} ${name}#${tag} Error: ${mmr_data.status}`);
 	}
 
-	// console.log(`[${mmr_data.data.currenttierpatched}] - ${mmr_data.data.ranking_in_tier} RR`);
-	// console.log(`${name}#${tag}`);
-	res.send(`${name}#${tag} [${mmr_data.data.currenttierpatched}] - ${mmr_data.data.ranking_in_tier} RR`);
-	myConsole.log(`${name}#${tag} [${mmr_data.data.currenttierpatched}] - ${mmr_data.data.ranking_in_tier} RR`);
+	if (req.query.onlyRank === "true") {
+		res.send(`${mmr_data.data.currenttierpatched} - ${mmr_data.data.ranking_in_tier} RR`);
+	} else res.send(`${name}#${tag} [${mmr_data.data.currenttierpatched}] - ${mmr_data.data.ranking_in_tier} RR`);
+
+	myConsole.log(`${region} ${name}#${tag} [${mmr_data.data.currenttierpatched}] - ${mmr_data.data.ranking_in_tier} RR`);
 });
 
 app.listen(port, () => {
-	myConsole.log(`Example app listening at http://localhost:${port}`)
+	myConsole.log(`Logged in.`)
 });
